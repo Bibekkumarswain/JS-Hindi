2c07be839f3e9ee8c78913397b4fad1f60b5f379
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-12 18:18:39 +0000

    commit all changes

--- 01_basics/04_comparision.js
+++ 01_basics/04_comparision.js
@@ -3,3 +3,19 @@ console.log(5>=4)
 console.log(2<6)
 console.log(7==3)
 console.log(8!=1)
+
+console.log("2">1)
+console.log("02">1)
+console.log(2>"1")
+console.log(2>"01")
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined >0)
+console.log(undefined < 0)
+
+console.log("2" === 2)
+

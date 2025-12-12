32de00a83347d9ab6f0a0978dc62f538f74fd9cf
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-12 19:36:46 +0000

    commit all

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,19 @@
+// According to official documents: datatypes are two types- i. Primitive ii. Non-primitive
+// In C, there is a concepts i. call by value...  ii.call by references
+//datatypes categorizing is produce by how the data's are store in memory or how you can access the data, based on this two we are divided the datatypes in two category.
+
+// Primitive Datatype
+
+// 7 types: String, Number, Boolean, null, undefined, Symbol(symbol is used to make unique to a value), BigInt
+
+// Reference (Non primitive Datatype)
+
+const score = 100
+const scoreValue=100.78
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')

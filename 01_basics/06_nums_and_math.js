8a66223470ba9004659f4d463f0667b496b47040
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-13 12:03:48 +0000

    commit

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -12,3 +12,12 @@ const otherNumber=23.9845
 console.log(otherNumber.toPrecision(3));
 
 console.log(hundreds.toLocaleString('en-IN'));
+
+// ========== MATH ==========
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4,6));
+console.log(Math.ceil(4,2));
+console.log(Math.floor(4,9));
+console.log(Math.min(4,3,9,6,5));
+console.log(Math.max(4,3,8,6));

b748a81d8fedbb99d1e979a57794d215a5bd3c71
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-13 07:24:58 +0000

    commit all changes

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,34 @@
+const name="Bibek"
+const repoCount=90
+
+//console.log(name+ repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('BibekBs')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('b'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString=gameName.slice(-8,4)
+console.log(anotherString);
+
+const newStringOne="   Bibek     "
+console.log(newStringOne)
+console.log(newStringOne.trim());
+
+const url = "https://github.com/Bibekkumarswain"
+
+console.log(url.replace('%20','-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'));
---
+++ 01_basics/stack-heapMemory.js
@@ -0,0 +1,20 @@
+let myYoutubeName = "Bibekkumar.com"
+
+let anotherName = myYoutubeName
+
+anotherName = "kumarBibek.com"
+
+console.log(myYoutubeName);
+
+console.log(anotherName);
+
+let userOne={
+    email: "userOne@gmail.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+userTwo.email = "Bibekkumar@google.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);

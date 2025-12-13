b82acc79f38c40704c33c0cf5de0101648afdfbd
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-13 11:40:30 +0000

    commit all changes

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,14 @@
+const score = 350
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+
+HTMLFormControlsCollection.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const otherNumber=23.9845
+
+console.log(otherNumber.toPrecision(3));
+
+console.log(hundreds.toLocaleString('en-IN'));

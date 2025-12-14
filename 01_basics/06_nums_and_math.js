4b29c19d62b9151e03d30c9927e896119fffdded
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-14 03:21:02 +0000

    commit

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -4,20 +4,29 @@ console.log(score);
 const balance = new Number(100)
 console.log(balance);
 
-HTMLFormControlsCollection.log(balance.toString().length);
+console.log(balance.toString().length);
 console.log(balance.toFixed(2));
 
 const otherNumber=23.9845
 
 console.log(otherNumber.toPrecision(3));
 
+const hundreds=100
 console.log(hundreds.toLocaleString('en-IN'));
 
 // ========== MATH ==========
-console.log(Math);
-console.log(Math.abs(-4));
-console.log(Math.round(4,6));
-console.log(Math.ceil(4,2));
-console.log(Math.floor(4,9));
-console.log(Math.min(4,3,9,6,5));
-console.log(Math.max(4,3,8,6));
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4,6));
+// console.log(Math.ceil(4,2));
+// console.log(Math.floor(4,9));
+// console.log(Math.min(4,3,9,6,5));
+// console.log(Math.max(4,3,8,6));
+
+console.log(Math.random());
+console.log(Math.random()*10+1);
+
+const min=10;
+const max=20;
+
+Math.random()

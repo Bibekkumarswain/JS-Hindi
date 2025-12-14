1ecf9f0b06a74ddbb76c592191b048e5a61b27fc
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-14 10:32:53 +0000

    commit

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,5 +1,7 @@
 // array
 //In JS,jab bhi aap array ke andar copy operation karenge,toh ye shallow copy banata hai.
-const myArr = [0,1,2,3,4,5];
+const myArr = [0,1,2,3,4,5]
 const myHeros=["john Cena" , "Roman"]
+
+const myArr2=new Array(1,2,3,4)
 console.log(myArr[3]);

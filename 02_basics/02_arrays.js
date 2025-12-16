681b97ddc570ac34daa3cf9632ee32bad5346ec6
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-16 11:38:32 +0000

    commit

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,8 @@
+const marvel_heros = ["Ironman","Spiderman","Thor"];
+
+const dc_heros = ["Superman","flash","Spiderman"];
+
+marvel_heros.push(dc_heros)
+
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][1]);

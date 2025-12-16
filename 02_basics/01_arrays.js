67fd533dbc1a8abedeecd7b17dc5073ad61479cd
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-16 08:50:38 +0000

    commit all changes

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -7,7 +7,30 @@ const myArr2=new Array(1,2,3,4)
 console.log(myArr[3]);
 
 //Array Methods
-myArr.push(6);
-myArr.push(7);
-myArr.pop(7);
-console.log(myArr);
+//myArr.push(6);
+//myArr.push(7);
+//myArr.pop(7);
+
+//myArr.unshift(9);
+//myArr.shift();
+
+//console.log(myArr.includes(9));
+//console.log(myArr.indexOf(3));
+
+//const newArr = myArr.join()
+
+//console.log(myArr);
+//console.log(newArr);
+//console.log(typeof newArr);
+
+// Slice, Splice
+
+console.log("A",myArr);
+
+const myn1 = myArr.slice(1,3);
+
+console.log(myn1)
+console.log("B",myArr)
+
+const myn2 = myArr.slice(1,3)
+console.log(myn2);

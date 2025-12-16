4a6a04433c78124db292c60759d3e681194185ff
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-16 13:40:03 +0000

    commit all changes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -6,3 +6,24 @@ tinderUser.name="Anmol"
 tinderUser.isLoggedIn = false
 
 console.log(tinderUser);
+
+const regularUser={
+    email:"some@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"Bibek",
+            lastname:"kumar"
+        }
+    }
+}
+
+//console.log(regularUser.fullname);
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1={1:"a",2:"b"}
+
+const obj2={3:"a", 4:"b"}
+const obj4 = {5:"a", 6:"b"}
+//const obj3 = {obj1, obj2}
+const obj3 = Object.assign({},obj1, obj2, obj4)
+console.log(obj3);

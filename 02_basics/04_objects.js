2a6a2171202ac559411495d5d2b73909d3a23677
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-18 13:17:47 +0000

    commit all changes

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -5,7 +5,7 @@ tinderUser.id="123abc"
 tinderUser.name="Anmol"
 tinderUser.isLoggedIn = false
 
-console.log(tinderUser);
+//console.log(tinderUser);
 
 const regularUser={
     email:"some@gmail.com",
@@ -25,5 +25,50 @@ const obj1={1:"a",2:"b"}
 const obj2={3:"a", 4:"b"}
 const obj4 = {5:"a", 6:"b"}
 //const obj3 = {obj1, obj2}
-const obj3 = Object.assign({},obj1, obj2, obj4)
-console.log(obj3);
+//const obj3 = Object.assign({},obj1, obj2, obj4)
+
+const obj3={...obj1,...obj2}
+//console.log(obj3);
+
+const users = [
+    {
+        id:1,
+        email:"bibek@gmail.com"
+    },
+    {
+        id:1,
+        email:"bibek@gmail.com"
+    },
+    {
+        id:1,
+        email:"bibek@gmail.com"
+    },
+]
+
+users[1].email
+//console.log(tinderUser);
+
+//console.log(Object.keys(tinderUser));
+
+//console.log(Object.values(tinderUser));
+
+//console.log(Object.entries(tinderUser));
+
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'));
+
+const course={
+    coursename:"JS in hindi",
+    price:"345",
+    courseInstructor:"Bibek"
+}
+
+//course.courseInstructor
+
+const{courseInstructor:Instructor}=course
+console.log(Instructor);
+
+/*const navbar = ((company))=>{
+
+}
+navbar(company="Bibek")*/
+

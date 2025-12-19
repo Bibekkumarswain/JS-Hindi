cb06c3d9b6b7ce065e34b78dea693d44005dc183
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-19 09:22:21 +0000

    commit all changes

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,13 @@
+function sayMyName(){console.log("B");
+console.log("I");
+console.log("B");
+console.log("E");
+console.log("K");}
+
+//sayMyName()
+
+function addTwoNumbers(number1, number2){
+    console.log(number1 + number2);
+}
+
+addTwoNumbers(4,5)

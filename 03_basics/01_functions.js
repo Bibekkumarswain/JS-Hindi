bc238a76c45d67a0290726e833d926718411d24c
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-20 12:55:12 +0000

    commit all changes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -6,8 +6,34 @@ console.log("K");}
 
 //sayMyName()
 
-function addTwoNumbers(number1, number2){
-    console.log(number1 + number2);
+function addTwoNumbers(number1, number2)//parameter
+{
+    //console.log(number1 + number2);
+    //let result = number1 + number2
+    //return result
+    return  number1 + number2
 }
 
-addTwoNumbers(4,5)
+addTwoNumbers(4,5);//Argument
+//addTwoNumbers(3,"4");
+//addTwoNumbers(3,"a");
+//addTwoNumbers(3,null);
+
+const result = addTwoNumbers(3,5)
+
+//console.log("Result: ", result);
+
+/*function loginUserMessage(userName){
+    return `${userName} just logged in`
+}
+//loginUserMessage("Bibek");
+console.log(loginUserMessage("Bibek"));*/
+
+function loginUserMessage(userName = "Someone"){
+    if(!userName){
+        console.log("Please enter a username");
+        return
+    }
+    return `${userName} just logged in`
+}
+console.log(loginUserMessage("Bibek"));

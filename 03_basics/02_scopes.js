380d476f19019490da27ff4569ed4f1c1c2ac1cd
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-21 06:25:34 +0000

    commit

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -2,15 +2,20 @@
 // const b=20;
 // var c=30;
 var c=300;
-let c=300;
+let a=300;
 
 if(true){
-    //ab maine jo ye block scope ke andar likha hai wo block scope hai. OR Iske bahar jo likh raha hu wo global scope.
+    //ab maine jo bhi ye block scope ke andar likha hai wo block scope hai. OR Iske bahar jo likh raha hu wo global scope.
     //Note: Global scope mein jo bhi aap likhenge wo value aap ke liye scope ke andar available hoti hai. Lekin scope ke andar jo value likhte hai wo bahar nhi jani chahiye...
     let a=10;
     const b=20;
     var c=30;
+    console.log("INNER: ", a);
 }//this curly braces is the scope
+
+/*for(let i=0; i<Array.length; i++){
+    const element =array[i];
+}*/
 
 // console.log(a);
 // console.log(b);

ba1aa92c4adf9d9cd70b0b75a5e68b20e20f1ce7
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-21 17:19:54 +0000

    commit all changes

--- 03_basics/03_Arrow.js
+++ 03_basics/03_Arrow.js
@@ -3,10 +3,46 @@ const user = {
     price: 999,
 
     welcomeMessage: function(){
-        console.log(`${this.username} , welcome to website`)
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
     }
     
 }
-user.welcomeMessage()
-user.username="sam"
-user.welcomeMessage()
+// user.welcomeMessage()
+// user.username="sam"
+// user.welcomeMessage()
+
+//console.log(this);//output is empty curly braces{}.
+
+/*function chai(){
+    let username="Bibek"
+    console.log(this.username);
+}
+chai();*/
+
+/*const chai=function(){
+    let username="Bibek"
+    console.log(this.username);
+}
+chai();*/
+
+/*const chai = ()=>{
+    let username = "Bibek"
+    console.log(this);
+    console.log(this.username);
+}
+chai();*/
+
+/*const addTwo=(num1, num2)=>{
+    return num1+num2// (Explicit return) - when we are using return keyword.
+} */
+
+//const addTwo = (num1, num2) => num1 + num2
+//const addTwo = (num1, num2) => (num1 + num2)//Implicit return
+
+const addTwo = (num1, num2) => ({username:"Bibek"})
+
+console.log(addTwo(3,5))
+
+const myArray = [2,5,3,7,8]
+myArray.forEach();

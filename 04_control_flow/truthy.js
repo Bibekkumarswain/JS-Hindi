b4700c35b7ad6bc906240e5fab51d5ac14b87d60
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-24 14:41:16 +0000

    commit all Changes

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -23,3 +23,18 @@ const emptyObj={}
 if(Object.keys(emptyObj).length===0){
     console.log("Object is empty");//Like this we can also detect & check the array is empty or not.
 }
+// Nullish Coalescing Operator(??): null undefined
+
+let val;
+//val=5??10
+//val=null??10
+//val=undefined??15
+val=null??10??20
+
+console.log(val);
+
+// Ternary Operator
+condition ? true : false
+
+const iceTeaPrice=100
+iceTeaPrice <= 80 ? console.log("less than 80") : console.log("more than 80");

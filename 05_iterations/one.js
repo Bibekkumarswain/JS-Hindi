1a803c552d9b7d8393a8719de244669c505925e2
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-24 18:49:09 +0000

    commit all changes

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -1,5 +1,33 @@
 // For Loop
 for(let index=0; index<=10; index++){
     const element=index;
-    console.log(element);
+    if(element==5){
+        //console.log("5 is best number");
+    }
+    //console.log(element);
+}
+//console.log(element);
+
+/*for(let index=0; index<=5; index++){
+    console.log(`Outer loop value: ${index}`);
+    for(let j=0; j<=5; j++){
+        console.log(`Inner loop value ${j} and inner loop ${index}`);
+        console.log(index + '*' + j + ' = ' +index*j);
+    }
+}*/
+
+let myArray=["flash","batman","superman"]
+for(let index=0; index<myArray.length; index++){
+    const element = myArray[index];
+    //console.log(element);
+}
+
+// Break & Continue Keyword
+for(let i=0;i<=20;i++){
+    if(i==5){
+        console.log(`Detected 5`);
+        continue;
+    
+    }
+    console.log(`value of i is ${index}`)
 }

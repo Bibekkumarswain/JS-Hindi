98fd1016c43e5270b33c69b018fc53d1e1982d5b
Bibek kumar swain <bibekkumarswain560@gmail.com>
2025-12-24 13:40:10 +0000

    commit all changes

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,11 +1,19 @@
 // IF Statement
-
 const isUserLoggedIn=true
 if(isUserLoggedIn){
-
+    console.log("successfully executed-1");
 }
-// <, >, <=, >=, ==, !=, 
+// <, >, <=, >=, ==, !=, ===
+/*if(2==="2"){
+    console.log("successfully executed-2");
+}*/
 
+/*const temperature=41
+if(temperature<50){
+    console.log("Temperature is less than 50");
+} else {
+    console.log("Temperature is greater than 50");
+}*/
 
 /* const score=200
 
@@ -21,11 +29,11 @@ const balance=1000;
 
 /*if(balance<500){
     console.log("less than 500");
-}else if(balance<750){
+} else if(balance<750){
     console.log("less than 750");
-} else if(balance<900){
+} else if (balance<900){
     console.log("less than 750")
-}else{
+} else {
     console.log("less than 1200")
 }*/
 
